edfd6653bdf805d6ca2ed7870f1ecb9c3ce38464
Alexandre Delattre <alexd6631@gmail.com>
2018-03-22 11:14:49 +0100

    Added addProduct test

--- common/src/test/kotlin/io/monkeypatch/bbl/kotlin/SaleTest.kt
+++ common/src/test/kotlin/io/monkeypatch/bbl/kotlin/SaleTest.kt
@@ -10,9 +10,19 @@ class SaleTest {
     fun `verify total sale price`() {
         val sale = Sale(listOf(
             Product("Product 1", 10.0),
-            Product("Product 1", 2.0, quantity = 2)
+            Product("Product 2", 2.0, quantity = 2)
         ))
 
+        assertEquals(14.0, sale.totalAmount())
+    }
+
+    @Test
+    fun `verify add product`() {
+        var sale = Sale()
+        assertEquals(0.0, sale.totalAmount())
+        sale = sale.addProduct(Product("Product 1", 10.0))
+        assertEquals(10.0, sale.totalAmount())
+        sale = sale.addProduct(Product("Product 2", 2.0, quantity = 2))
         assertEquals(14.0, sale.totalAmount())
     }
 }
